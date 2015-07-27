759e48823aee9f4f931e75bf0212812fcac5c428
Rachel Denison <r.n.denison@gmail.com>
2015-07-27 19:09:59 -0400

    added flexible purplab naming (purplab or purplab-1) to pathToCarrascoExpts.m

--- TADetectDiscrim/pathToCarrascoExpts.m
+++ TADetectDiscrim/pathToCarrascoExpts.m
@@ -1,3 +1,11 @@
 function exptspath = pathToCarrascoExpts
 
-exptspath = '/Volumes/purplab-1/EXPERIMENTS/1_Current Experiments/Rachel';
+if exist('/Volumes/purplab','dir')
+    purpdir = 'purplab';
+elseif exist('/Volumes/purplab-1','dir')
+    purpdir = 'purplab-1';
+else
+    error('cannot find purplab')
+end
+
+exptspath = sprintf('/Volumes/%s/EXPERIMENTS/1_Current Experiments/Rachel',purpdir);
