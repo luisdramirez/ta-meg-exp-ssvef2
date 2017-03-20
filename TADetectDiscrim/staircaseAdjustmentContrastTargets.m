a0e34c65d8781b9afe1a4df295ff30b6cbcebc0a
Rachel Denison <r.n.denison@gmail.com>
2017-03-20 16:06:37 -0400

    staircase every 2 runs

--- TADetectDiscrim/staircaseAdjustmentContrastTargets.m
+++ TADetectDiscrim/staircaseAdjustmentContrastTargets.m
@@ -1,4 +1,4 @@
-function contrasts = staircaseAdjustmentContrastTargets(pedestal, contrasts, discrims)
+function contrasts = staircaseAdjustmentContrastTargets(pedestal, contrasts, perfs)
 %
 % function staircaseAdjustment(contrasts, discrims)
 %
@@ -9,7 +9,7 @@ function contrasts = staircaseAdjustmentContrastTargets(pedestal, contrasts, dis
 %% report current values
 fprintf('pedestal contrast: %.2f\n', pedestal)
 fprintf('original contrasts: [%.2f %.2f]\n', contrasts(1), contrasts(2))
-fprintf('discrimination: [%d %d]%%\n\n', round(discrims(1)*100), round(discrims(2)*100))
+fprintf('performance: [%d %d]%%\n\n', round(perfs(1)*100), round(perfs(2)*100))
 
 %% contrast decrement
 c = logspace(-2,0,30);
@@ -17,15 +17,15 @@ c = logspace(-2,0,30);
 c = c(1:pIdx-1);
 
 [val, cIdx] = min(abs(c-contrasts(1))); % find c closest to contrast
-if discrims(1) > 0.90
+if perfs(1) > 0.90
     if cIdx < numel(c)
         cIdx = cIdx + 1; % higher is harder
     end
-elseif discrims(1) < 0.55
+elseif perfs(1) < 0.65
     if cIdx > 1
         cIdx = cIdx - 1; % lower is easier
     end
-%     if discrims(1) < 0.55 % extra boost for near chance performance
+%     if perfs(1) < 0.55 % extra boost for near chance performance
 %         if cIdx > 1
 %             cIdx = cIdx - 1; % lower is easier
 %         end
@@ -41,15 +41,15 @@ c = logspace(-2,0,30);
 c = c(pIdx+1:end);
 
 [val, cIdx] = min(abs(c-contrasts(2))); % find c closest to contrast
-if discrims(2) > 0.90
+if perfs(2) > 0.90
     if cIdx > 1
         cIdx = cIdx - 1; % lower is harder
     end
-elseif discrims(2) < 0.55
+elseif perfs(2) < 0.65
     if cIdx < numel(c)
         cIdx = cIdx + 1; % higher is easier
     end
-%     if discrims(2) < 0.55 % extra boost for near chance performance
+%     if perfs(2) < 0.55 % extra boost for near chance performance
 %         if cIdx < numel(c)
 %             cIdx = cIdx + 1; % higher is easier
 %         end
