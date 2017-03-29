c8194cc538287484824e4ac175303d2495894edb
Rachel Denison <r.n.denison@gmail.com>
2017-03-29 13:15:02 -0400

    analyzing target contrast, reordering plotLevels, fixed bug in block_gen for generating target contrast types in 2-target always experiment

---
+++ TADetectDiscrim/rd_plotTADetectDiscrimContrast.m
@@ -0,0 +1,56 @@
+% plot performance and contrast by run
+
+%% initial analysis
+subject = 'rd';
+runs = 66:70;
+[acc, ~, responseData_all, responseData_labels] = TADetectDiscrim_analysis(subject, runs, [], 1);
+rd_supertitle2(sprintf('%s, runs %d-%d', subject, runs(1), runs(end)))
+
+%% mean contrast
+10.^mean(log10(acc.targetContrast))
+
+10.^mean(log10(acc.targetContrast(end/2+1:end,:)))
+
+%% overall accuracy
+% runIdx = find(strcmp(responseData_labels, 'run number'));
+% correctIdx = find(strcmp(responseData_labels, 'correct'));
+% nRuns = numel(unique(responseData_all(:,runIdx)));
+% correct = responseData_all(:,correctIdx);
+% correct(correct==-1) = 0;
+% 
+% for iRun = 1:nRuns
+%     w = responseData_all(:,runIdx)==iRun;
+%     correctByRun(iRun) = nanmean(correct(w));
+% end
+% correctOverall = mean(correctByRun);
+
+% quick and dirty
+accvals = acc.Discrim1_all;
+correctByRun = [accvals'*[.75 .25 0 0]' accvals'*[0 0 .75 .25]'];
+correctOverall = mean(correctByRun)
+
+%% plot
+ylims = [0 1];
+figure
+subplot(1,2,1)
+hold on
+plot(accvals(1:2,:)')
+plot(correctByRun(:,1),'k--')
+ylim(ylims)
+xlabel('run')
+ylabel('discrim/detected')
+title('T1')
+subplot(1,2,2)
+hold on
+plot(accvals(3:4,:)')
+plot(correctByRun(:,2),'k--')
+ylim(ylims)
+xlabel('run')
+title('T2')
+rd_supertitle2(sprintf('%s, runs %d-%d', subject, runs(1), runs(end)))
+
+figure
+plot(acc.targetContrast)
+xlabel('run')
+ylabel('target contrast')
+
