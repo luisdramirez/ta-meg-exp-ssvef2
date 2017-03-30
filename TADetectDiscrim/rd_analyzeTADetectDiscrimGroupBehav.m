f8a0725e73fa2e090e630a47cd23d762c899a135
Rachel Denison <r.n.denison@gmail.com>
2017-03-30 19:58:04 -0400

    group analysis

--- TADetectDiscrim/rd_analyzeTADetectDiscrimGroupBehav.m
+++ TADetectDiscrim/rd_analyzeTADetectDiscrimGroupBehav.m
@@ -1,14 +1,9 @@
 % rd_analyzeTADetectDiscrimGroupBehav.m
 
 %% setup
-exptDir = '/Volumes/DRIVE1/DATA/rachel/MEG/TADetectDiscrim/MEG';
+exptDir = pathToExpt('data');
 
-subjects = {'R0817_20150504', 'R0973_20150727', 'R0974_20150728', ...
-    'R0861_20150813', 'R0504_20150805', 'R0983_20150813', ...
-    'R0898_20150828', 'R0436_20150904', 'R1018_20151118', ...
-    'R1019_20151118','R1021_20151120','R1026_20151211', ...
-    'R0852_20151211','R1027_20151216','R1028_20151216',...
-    'R1029_20151222'}; % N=16
+subjects = {'rd','lr','mj','af'};
 
 nSubjects = numel(subjects);
 
