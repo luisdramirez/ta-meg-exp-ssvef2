b160378c2b88f07063df2ebe92fc905ca635369b
luisdramirez <ldr25@nyu.edu>
2017-04-06 17:26:00 -0400

    can run without catch trials.

--- TADetectDiscrim/taAutoRuns.m
+++ TADetectDiscrim/taAutoRuns.m
@@ -4,6 +4,7 @@ function taAutoRuns(runs)
     for iRun = 1:length(runs)
         curr_run = runs(iRun);
         rd_MEG_TAPilot(curr_run, 'taDetectDiscrim')
+        sca
     end
     
 %implement graceful exit
