d4baec3d54062e82635d0115a1c6b2b7f2dad0aa
luisdramirez <ldr285@nyu.edu>
2017-04-11 20:25:50 -0400

    comment changes. taAutoRuns foundation updates.

--- TADetectDiscrim/rd_plotTADetectDiscrimContrast.m
+++ TADetectDiscrim/rd_plotTADetectDiscrimContrast.m
@@ -1,9 +1,9 @@
 % plot performance and contrast by run
 
 %% initial analysis
-subject = 'af';
-runs = 121:130;
-date = '20170407';
+subject = 'lr';
+runs = 101801;
+date = '20170410';
 plotLevel = 1;
 saveFile = 0;
 saveFigs = 0;
