75bf8a18e8c4c89ded82ec7ea2f8add08a009eac
luisdramirez <ldr25@nyu.edu>
2017-10-04 15:19:09 -0400

    updated analysis path

--- TADetectDiscrim/TADetectDiscrim_analysis.m
+++ TADetectDiscrim/TADetectDiscrim_analysis.m
@@ -45,9 +45,9 @@ end
 
 %% combine responseData for all runs
 % get the data from the server using pathToExpt
-rootDir = pathToMEGExpt;
+% rootDir = pathToMEGExpt;
 % rootDir = pathToExpt;
-%rootDir = '~/Desktop/Luis/ta-meg-exp-ssvef2/TADetectDiscrim/';
+rootDir = '~/Desktop/Luis/ta-meg-exp-ssvef2/TADetectDiscrim';
 % rootDir = '/Volumes/purplab/EXPERIMENTS/1_Current Experiments/Luis/ta-meg-exp-ssvef2/TADetectDiscrim';
 %rootDir = '/Users/luisramirez/Documents/CarrascoLabMEG/ta-meg-exp-ssvef2/TADetectDiscrim';
 % rootDir = pwd;
