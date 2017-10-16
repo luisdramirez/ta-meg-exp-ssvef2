54c26835346915e9fbee153975545670a68671ae
Rachel Denison <r.n.denison@gmail.com>
2017-10-16 11:42:35 -0400

    working on block_gen3

--- TADetectDiscrim/block_gen3.m
+++ TADetectDiscrim/block_gen3.m
@@ -2,7 +2,24 @@
 
 nContrastT1 = 4;
 nContrastT2 = 4;
-nCV = 4;
+nV = 4;
 nT = 2;
 
-a = fullfact([nContrastT1 nContrastT2 nCV nT])
+a = fullfact([nContrastT1 nContrastT2 nV nT]);
+
+validity = a(:,3);
+validity(validity~=4) = 1;
+validity(validity==4) = 2;
+a(:,3) = validity;
+
+idx = randperm(size(a,1));
+b = a(idx,:);
+
+
+
+
+
+contrastT1 = a(:,1);
+contrastT2 = a(:,2);
+validity = a(:,3);
+target = a(:,4);
