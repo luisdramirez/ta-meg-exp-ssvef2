d0e6f50653a94c617517462e342a1308eaadaf92
Rachel Denison <r.n.denison@gmail.com>
2017-10-16 13:47:48 -0400

    finished block_gen3

--- TADetectDiscrim/contrastToTypePedestal.m
+++ TADetectDiscrim/contrastToTypePedestal.m
@@ -3,8 +3,12 @@ function [tt, tp] = contrastToTypePedestal(contrastLevels)
 lut = [1 1; 1 2; 2 1; 2 2];
 
 for i = 1:length(contrastLevels)
-    tp(i) = lut(contrastLevels(i),1);
-    tt(i) = lut(contrastLevels(i),2);
+    if contrastLevels(i)==0
+        tp(i) = NaN;
+        tt(i) = NaN;
+    else
+        tp(i) = lut(contrastLevels(i),1);
+        tt(i) = lut(contrastLevels(i),2);
+    end
 end
 
-end
